946be388adbe5ff5213a8a34090e81943fa3ee74
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-01-27 22:31:01 +0530

    Added Valid First Name Program For Test

---
+++ RegexProgram/ValidFirstNameUC_1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexProgram
+{
+    public  class ValidFirstNameUC_1
+    {
+        public static string REGEX_VALIDFIRSTNAME = "^[A-Z]{1}[A-Z a-z ]{5}$";
+
+
+        public bool validateValidfirstName(string validfirstname)
+        {
+            return Regex.IsMatch(validfirstname, REGEX_VALIDFIRSTNAME);
+        }
+    }
+}
--- RegexProgramTest/UnitTest1.cs
+++ RegexProgramTest/UnitTest1.cs
@@ -1,11 +1,19 @@
+using RegexProgram;
+
 namespace RegexProgramTest
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+
+        public void ValidFirstName()
         {
+            RegexProgram.ValidFirstNameUC_1 validFirstNameUC_1 = new RegexProgram.ValidFirstNameUC_1();
+
+            string REGEX_VALIDFIRSTNAME = validFirstNameUC_1.ToString();
+
+            Assert.AreEqual(REGEX_VALIDFIRSTNAME, REGEX_VALIDFIRSTNAME);
         }
     }
 }
